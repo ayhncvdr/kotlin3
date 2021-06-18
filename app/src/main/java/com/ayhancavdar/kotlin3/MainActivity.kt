90f2b955107afc560acbcd1ae2afe65b79d60965
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-06-18 12:20:16 +0300

    first commit to dev/ayhan2

--- app/src/main/java/com/ayhancavdar/kotlin3/MainActivity.kt
+++ app/src/main/java/com/ayhancavdar/kotlin3/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         anotherFunc()
         mySum(15,36)
 
-       var y= myMultiply(8,36)
+       var y= myMultiply(9,37)
         println(y)
 
         var homer = Simpson(50,"Homer Simpson", "Nuclear engineer");
