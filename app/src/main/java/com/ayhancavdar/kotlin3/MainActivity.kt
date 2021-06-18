eaab48b9102def8038d979b12b66595fca7d627c
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-06-18 12:07:51 +0300

    first commit

---
+++ app/src/main/java/com/ayhancavdar/kotlin3/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.ayhancavdar.kotlin3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log.println
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("onCreate got executed")
+        test()
+        anotherFunc()
+        mySum(15,36)
+
+       var y= myMultiply(8,36)
+        println(y)
+
+        var homer = Simpson(50,"Homer Simpson", "Nuclear engineer");
+        var bart = Simpson(10,"Bart Simpson", "Student")
+        println(homer.age)
+        homer.hairColor="yellow"
+        println(homer.hairColor)
+        bart.changeHairColorToBlack()
+        println(bart.hairColor)
+
+    }
+    fun test(){
+        println("test function got executed")
+    }
+    fun anotherFunc(){
+        test()
+    }
+    fun mySum( a: Int, b:Int){
+        println(a+b)
+
+    }
+    fun myMultiply(a:Int, b:Int) : Int{
+        return a*b
+    }
+    fun newFunction(view: View){
+        println("button is clicked")
+    }
+
+    //Nullability
+    var myString = "Ayhan"
+    lateinit var myOtherString : String
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/ayhancavdar/kotlin3/Simpson.kt
@@ -0,0 +1,26 @@
+package com.ayhancavdar.kotlin3
+
+class Simpson (var age : Int, var name:String, var job: String){
+    var hairColor=""
+
+    fun changeHairColorToBlack(){
+        this.hairColor="Black"
+    }
+
+    /*var age =0;
+    var name =""
+    var job = ""
+    var hairColor: String
+
+    //constructor
+    constructor(age: Int, name:String, job: String, hairColor: String){
+        println("a new object is created")
+        this.age=age;
+        this.name=name;
+        this.job=job;
+        this.hairColor=hairColor;
+    }
+*/
+
+
+}
